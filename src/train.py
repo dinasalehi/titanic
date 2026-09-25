ffe19e7952769e6ee4efb2bfe28e490815c3ccc1
Dina <salehi.dina1385@gmail.com>
2026-09-25 10:54:40 +0330

    Define some models

--- src/train.py
+++ src/train.py
@@ -26,3 +26,20 @@ def standardisation(x_train,x_test):
     x_test_scaled=scaler.transform(x_test)
 
     return x_train_scaled,x_test_scaled
+
+def tree_models(model,param_grid,x_train,x_test,y_train,y_test):
+    """ Train and tune a tree-based regression model using GridSearchCV."""
+
+    grid_search=GridSearchCV(
+        estimator=model,
+        param_grid=param_grid,
+        scoring='accuracy',
+        return_train_score=True,
+        n_jobs=-1)
+    grid_search.fit(x_train,y_train) 
+    best_model=grid_search.best_estimator_
+    y_pred=best_model.predict(x_test)
+    acc=accuracy_score(y_pred,y_test)
+    return best_model,y_pred,acc
+   
+   
